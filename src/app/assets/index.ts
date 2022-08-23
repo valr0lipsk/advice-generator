17da73af4af5bc7bd7dd966c3c262e6f8a53ba33
Valeryia Lipskaya <valery.lipskaya03@gmail.com>
2022-08-23 11:23:47 +0300

    svg icons added

---
+++ src/app/assets/index.ts
@@ -0,0 +1,9 @@
+import { default as iconDice } from '!!raw-loader?!./icon-dice.svg';
+import { default as dividerDesktop } from '!!raw-loader?!./pattern-divider-desktop.svg';
+import { default as dividerMobile } from '!!raw-loader?!./pattern-divider-mobile.svg';
+
+export const icons: Record<string, string> = {
+  iconDice,
+  dividerMobile,
+  dividerDesktop,
+};
