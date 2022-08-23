00a807dbfa83697aaa47a77476f47bb8804c31ea
Valeryia Lipskaya <valery.lipskaya03@gmail.com>
2022-08-23 11:24:42 +0300

    backnd service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { SvgIconComponent } from './components/svg-icon/svg-icon.component';
+import { AdviceComponent } from './components/advice/advice.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, SvgIconComponent, AdviceComponent],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/backend.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface Advice {
+  id: string;
+  advice: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BackendService {
+  public constructor(private readonly http: HttpClient) {}
+
+  public getAdvice$(): Observable<Advice> {
+    return this.http.get<Advice>('https://api.adviceslip.com/advice');
+  }
+}
